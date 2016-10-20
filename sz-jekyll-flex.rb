d41c908e58aee32fb9b614a864706cbdeb2f8670
StartZeroGnu <startzerognu@gmail.com>
2016-10-20 12:54:52 +0200

    Add ask & name
    add "ask" for use with "name" and rename "template" after unzip

--- sz-jekyll-flex.rb
+++ sz-jekyll-flex.rb
@@ -1,5 +1,13 @@
-say "Hello from the sz-jekyll-flex theme"
+say "Hello from the sz-jekyll-flex template"
+
+## use user answer for unzip and rename (sz-jekyll-flex/template)
+name  = ask "Name of the theme", "blog"
 
 ## use template repo e.g. github.com/quikstart/jekyll-starter-theme
-
 use "StartZeroGnu/sz-jekyll-flex"
+
+config do |c|
+  
+  c.name     = name
+  
+end
