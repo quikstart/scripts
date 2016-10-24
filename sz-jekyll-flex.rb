12776ec91324c494770edc1a180ba7d6c621e0ba
StartZeroGnu <startzerognu@gmail.com>
2016-10-24 09:20:34 +0200

    Update sz-jekyll-flex.rb

--- sz-jekyll-flex.rb
+++ sz-jekyll-flex.rb
@@ -1,7 +1,7 @@
 say "Hello from the sz-jekyll-flex template"
 
 ## use user answer for unzip and rename (sz-jekyll-flex/template)
-name  = ask "Name of the theme", "blog"
+name  = ask "Name of the template", "blog"
 
 ## use template repo e.g. github.com/quikstart/jekyll-starter-theme
 use "StartZeroGnu/sz-jekyll-flex"
